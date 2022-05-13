7ac277aff73dc20bea32f57fcf0f62303b56799f
VicenteGL <garcia20374@iesmarenostrum.com>
2022-05-13 20:52:29 +0200

    El miercoles

---
+++ RefactorizacionVicente/Fecha/Fecha.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fecha
+{
+    public class Fecha
+    {
+        public int day;
+        public int month;
+        public int year;
+        //TODO validar los valores introducidos
+        /// <summary>
+        /// Constructor de Fecha sin parámetros
+        /// Se establecen los valores a 1
+        /// </summary>
+        public Fecha()
+        {
+            day = 1;
+            month = 1;
+            year = 1;
+        }
+        /// <summary>
+        /// Constructor de Fecha con 3 parámetros
+        /// Si algún parámetro no es correcto se establece a 1
+        /// </summary>
+        /// <param name="mes">Mes</param>
+        /// <param name="anyo">Anyo (entre 1 y 2500)</param>
+        /// <param name="dia">Dia</param>
+        /// <param name="bi">Indica si es bisiesto</param>
+        public Fecha(int mes, int anyo, int dia, bool bi)
+        {
+            if (anyo >= 1 && anyo <= 2500)
+            {
+                this.year = anyo;
+            }
+            else
+            {
+                this.year = 1;
+            }
+            bool bisiesto = EsBisiesto();
+            
+            if (mes >= 1 && mes <= 12)
+                this.month = mes;
+            else
+                this.month = 1;
+            int diasMes = 0;
+            switch (month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    diasMes = 31;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    diasMes = 30;
+                    break;
+                case 2: // verificación de año bisiesto
+                    if (bisiesto)
+                        diasMes = 29;
+                    else
+                        diasMes = 28;
+                    break;
+            }
+
+            if (dia >= 1 && dia <= diasMes)
+                this.day = dia;
+            else
+                this.day = 1;
+        }
+
+        public bool EsBisiesto()
+        {
+            bool bisiesto = false;
+            if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
+                bisiesto = true;
+            else bisiesto = false;
+            return bisiesto;
+        }
+        /// <summary>
+        /// Devuelve un string con la fecha en formato dia/mes/anyo
+        /// </summary>
+        /// <remarks> la palabra clave override indica que se sobreescribe el
+        //metodo ToString</remarks>
+        /// <returns>un string con la fecha en formato dia/mes/anyo</returns>
+        public override string ToString()
+        {
+            return day + "/" + month + "/" + year;
+        }
+    }
+}
---
+++ RefactorizacionVicente/Fecha/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace Fecha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            test1();//Fecha correcta, año bisiesto
+
+            test2();//Fecha correcta, año no bisiesto
+
+            test3();//Fecha con valores incorrectos
+
+            test4();//Fecha con asignación incorrecta de valores erroneos
+        }
+
+        /// <summary>
+        /// Fecha correcta, año bisiesto
+        /// </summary>
+        static void test1()
+        {
+            Fecha fecha1 = new Fecha(12, 2012, 4, false);
+            Console.WriteLine("Fecha 1: " + fecha1.ToString());
+            if (fecha1.EsBisiesto())
+                Console.WriteLine("El año " + fecha1.year + " es bisiesto");
+            else
+                Console.WriteLine("El año " + fecha1.year + " no es bisiesto");
+        }
+
+        /// <summary>
+        /// Fecha correcta, año no bisiesto
+        /// </summary>
+        static void test2()
+        {
+            Fecha fecha2 = new Fecha(10, 2013, 4, false);
+            Console.WriteLine("Fecha 2: " + fecha2.ToString());
+            if (fecha2.EsBisiesto())
+                Console.WriteLine("El año " + fecha2.year + " es bisiesto");
+            else
+                Console.WriteLine("El año " + fecha2.year + " no es bisiesto");
+        }
+
+        /// <summary>
+        /// Fecha con valores incorrectos
+        /// </summary>
+        static void test3()
+        {
+            Fecha fecha3 = new Fecha(13, -4, 4, false);
+            Console.WriteLine("Fecha 3: " + fecha3.ToString());
+        }
+
+        /// <summary>
+        /// Fecha con asignación incorrecta de valores erroneos
+        /// </summary>
+        static void test4()
+        {
+            Fecha fecha4 = new Fecha();
+            fecha4.day = 67;
+            fecha4.month = 80;
+            fecha4.year = 3678;
+            Console.WriteLine("Fecha 4: " + fecha4.ToString());
+        }
+    }
+}
+//MAL!!
